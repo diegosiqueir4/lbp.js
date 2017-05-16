cbed752195f69d6af7554e0528dfd107006c78c6
Jeffrey Witt <jcwitt@JeffsComputer.local>
2017-05-15 21:05:28 -0400

    major progress wiring up library to scta sparql endpoint; query object created along with value and values methods on Resource object

--- bin/test.js
+++ bin/test.js
@@ -1,4 +1,33 @@
 var Lbp = require("../lib/")
 
-r = new Lbp.Manifestation("NEW TEST")
-console.log(r.transcriptions());
+// r = new Lbp.Query("http://scta.info/resource/lectio1");
+// var query = r.immediate_props();
+// var result = r.run(query);
+//
+// result.then(function(data){
+//   data = JSON.parse(data)
+//   var results = data.results.bindings;
+//   // the code you're looking for
+//   var needle = 'http://www.loc.gov/loc.terms/relators/AUT';
+//
+// // iterate over each element in the array
+// var value = '';
+//
+// for (var i = 0; i < results.length; i++){
+//   // look for the entry with a matching `code` value
+//   if (results[i].p.value === needle){
+//     value = results[i].o.value;
+//   }
+// }
+// console.log(value);
+//   //console.log(data.results.bindings)
+//
+// });
+
+ r = new Lbp.Expression("http://scta.info/resource/lectio1");
+ //result = r.values("http://scta.info/property/hasManifestation");
+ result = r.manifestations()
+
+ result.then(function(data){
+   console.log(data);
+ });
